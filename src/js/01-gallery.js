9b3855abd34dc9019bbb6e0cdc1e189c2bcd0e87
Serhii-Khrapun <Sergiy.Khrapun@gmail.com>
2021-11-02 22:25:50 +0200

    закінчив 1 завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import simpleLightbox from "simpleLightbox"
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+
+// import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryElement = document.querySelector(".gallery");
+
+const createImg = galleryItems
+  .map(({preview, original,description}) =>
+    
+      `<a class="gallery__item" href="${original}">
+     <img class="gallery__image" src="${preview}" 
+    alt="${description}" />
+</a>`
+  )
+  .join(" ");
+
+galleryElement.insertAdjacentHTML("afterbegin", createImg);
+const lightbox = new SimpleLightbox('.gallery a', { captionsData:'alt', captionDelay:250});
+
+const galleryContainer = document.querySelector(".gallery");
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+function onGalleryContainerClick (event) {
+    event.preventDefault();
+    if(event.target.tagName !== "IMG"){
+      return;
+    }
+ }
